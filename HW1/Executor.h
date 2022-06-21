6bba989859ce23a32520edd0066e908da62da927
SEWMe <106031669+SEWMe@users.noreply.github.com>
2022-06-21 10:34:12 -0600

    Create Executor.h

---
+++ HW1/Executor.h
@@ -0,0 +1,17 @@
+//
+// Created by AFHIR on 5/19/2022.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef HOMEWORK1_EXECUTOR_H
+#define HOMEWORK1_EXECUTOR_H
+
+
+double get_running_ratio();
+int get_iteration_no(int rand);
+int get_arr_size(int rand);
+char get_arr_val(int rand);
+float ratio(char *arr, int size, int *maxCountPointer);
+
+#endif //HOMEWORK1_EXECUTOR_H
